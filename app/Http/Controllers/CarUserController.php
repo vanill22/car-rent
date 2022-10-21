bc6f17ebaf8e0ba2b914c7b080139892274c1173
Ivan Filippov <ivan@bik-agency.ru>
2022-10-21 18:44:20 +0300

    correct docker user and group, make models, controller

---
+++ app/Http/Controllers/CarUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CarUser;
+use Illuminate\Http\Request;
+
+class CarUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CarUser  $carUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CarUser $carUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CarUser  $carUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CarUser $carUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CarUser  $carUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CarUser $carUser)
+    {
+        //
+    }
+}
